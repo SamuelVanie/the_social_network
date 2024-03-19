3856e8dce102cc91d8ba10baa7457fa423151e84
Samuel Michael Vanie <samuelmichaelvanie@Samuels-MacBook-Air.local>
2024-03-19 18:32:32 +0100

    feat: Add the callback function and message handler to front

--- frontend/ui/ts/RecieveMessage.js
+++ frontend/ui/ts/RecieveMessage.js
@@ -1,22 +1,21 @@
 const button = document.getElementById("submit");
-var chat
+var chat;
 
-button.addEventListener('click', (e) => display_message(e), false);
+button.addEventListener("click", (e) => display_message(e), false);
 
 async function display_message(e) {
-    e.preventDefault();
-    invoke('get_message', {}).then((response) => {
-        let author = response.author;
-        let content = response.message;
-        let messageBloc = document.createElement('div');
-        messageBloc.innerText = author + ': ' + content;
-        console.log(chat)
-        chat.appendChild(messageBloc);
-    })
+  e.preventDefault();
+  invoke("susbcribe_to_channel", { channel_id: 1 }).then((response) => {
+    console.log(response.content);
+  });
 }
 
 function onReady() {
-    chat = document.getElementById('convo-chat');
+  chat = document.getElementById("convo-chat");
 }
 
-window.addEventListener("load", () => onReady())
+window.addEventListener("load", () => onReady());
+
+window.addEventListener("new_message", (event) => {
+  console.log(event.payload);
+});
